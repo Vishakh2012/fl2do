439915c126b357cdf42855371877a9b4fbf24a17
[Vishwa Prakash] <[vishakpro2002@gmail.com]>
2022-11-08 00:01:00 +0530

    FEAT: - create a class inheritted from FlaskForm - add form to templates/base.html using wtforms - changes in the footer - add svg to the footer - chnage description in the footer - change color of link in the footer to secondary use bootstrap
    TEST:
    - Run the program on the local server
    - Test the code by putting the values into the form and submitting the
      form
    
    NEXT:
    
    - sub_task checkboxes check boxes rendered using wtforms
    - add javascript so that a submit button is not needed
    

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 
 
+
 db=SQLAlchemy()
 app = Flask(__name__)
 
@@ -15,9 +16,9 @@ app.config['SECRET_KEY']= '6716c1c7a2a42caadadb169af7cc9df4'
 class task_form(FlaskForm):
     task = StringField('enter your task')
     task_desc =StringField('decribe your task briefly')
-    sub_task1 =StringField('sub_task1')
-    sub_task2 =StringField('sub_task2')
-    sub_task3 =StringField('sub_task3')
+    sub_task1 =StringField('enter the subtask')
+    sub_task2 =StringField('enter the subtask')
+    sub_task3 =StringField('enter the subtask')
     submit = SubmitField('Submit')
  
     
@@ -37,16 +38,27 @@ class Todo(db.Model):
 
 
 
-@app.route("/update/", methods=["POST","GET"])
+@app.route("/")
 def task():
     form = task_form()
-    if form.validate_on_submit():
-        return redirect("/success/")
-    return render_template("index.html", form = form)
+    allTodo = Todo.query.all()
+    return render_template("index.html", form = form,allTodo=allTodo)
         
-@app.route("/success/")
-def success():
-    return render_template("success.html")
+@app.route("/update/", methods=["POST","GET"])
+def add_to_table():
+    form = task_form()
+    if request.method == 'POST' and form.validate_on_submit():
+        todo = Todo(
+            task_name = form.task.data,
+            task_desc = form.task_desc.data,
+            sub_task1 = form.sub_task1.data,
+            sub_task2 = form.sub_task2.data,
+            sub_task3 = form.sub_task3.data
+            )
+        db.session.add(todo)
+        db.session.commit()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True,use_reloader = False)
