0267ced7115a6323f0f45cc2e9e5525981a9b3c4
[Vishwa Prakash] <[vishakpro2002@gmail.com]>
2022-11-11 01:38:00 +0530

    c

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField, BooleanField
+from wtforms.validators import InputRequired
 
 
 
@@ -10,6 +12,19 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fl2do.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY']= '6716c1c7a2a42caadadb169af7cc9df4'
+
+class task_form(FlaskForm):
+    task = StringField('enter your task',validators = [InputRequired('please enter the task')])
+    task_desc = StringField('decribe your task briefly')
+    sub_task1 = StringField('enter the subtask',validators = [InputRequired('please enter the task')])
+    sub_task2 = StringField('enter the subtask',validators = [InputRequired('please enter the task')])
+    sub_task3 = StringField('enter the subtask',validators = [InputRequired('please enter the task')])
+    submit = SubmitField('Submit')
+ 
+
+
+       
 
 with app.app_context():
     db.init_app(app)
@@ -24,32 +39,48 @@ class Todo(db.Model):
     declared_at = db.Column(db.DateTime, default=datetime.utcnow)
     
 
-@app.route("/")
-def show():
-    allTodo = Todo.query.all()
-    return render_template('index.html',allTodo = allTodo)
 
-@app.route("/update/", methods=["POST","GET"])
-def create_task():
-    if request.method == 'POST':
-        task_name = request.form["task_name"]
-        task_desc = request.form["task_desc"]
-        sub_task1 = request.form["sub_task1"]
-        sub_task2 = request.form["sub_task2"]
-        sub_task3 = request.form["sub_task3"]
+class subtask(FlaskForm):
+    cst1 = BooleanField("form.sub_task1.data",default = False)
+    cst2 = BooleanField("form.sub_task2.data",default = False)
+    cst3 = BooleanField("form.sub_task3.data",default = False)
+
+
+@app.route("/")
+def task():
+    sub_form = subtask()
+    form = task_form()
+    allTodo = Todo.query.all()
+    return render_template("index.html", form = form,allTodo=allTodo,sub_form=sub_form)
         
+@app.route("/delete/<int:sno>", methods=["POST","GET"])
+def delete_to_table(sno):
+    del_task = Todo.query.filter_by(SNo = sno).first()
+    db.session.delete(del_task)
+    db.session.commit()
+    return redirect("/")
+    
+    
+    
+    
+    
+    
+@app.route("/update/", methods=["POST","GET"])
+def add_to_table():
+    form = task_form()
+    if request.method == 'POST' and form.validate_on_submit():
         todo = Todo(
-            task_name=task_name,
-            task_desc=task_desc,
-            sub_task1 = sub_task1,
-            sub_task2 = sub_task2,
-            sub_task3 = sub_task3
+            task_name = form.task.data,
+            task_desc = form.task_desc.data,
+            sub_task1 = form.sub_task1.data,
+            sub_task2 = form.sub_task2.data,
+            sub_task3 = form.sub_task3.data
             )
         db.session.add(todo)
         db.session.commit()
     return redirect("/")
-        
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,use_reloader = False)
 
